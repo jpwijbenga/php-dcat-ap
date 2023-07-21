14ab8ce55dc0d2aa62456152c4478fde4b80ecd3
Michiel Stornebrink <michielstornebrink@gmail.com>
2023-07-21 22:22:30 +0200

    Add mandatory properties for Catalogue

--- src/DCAT/Catalogue.php
+++ src/DCAT/Catalogue.php
@@ -10,4 +10,42 @@ use PHP_DCAT_AP\Attribute\URI;
 #[URI('dcat:Catalog')]
 class Catalogue
 {
+    /** @var string[] */
+    protected static $REQUIRED_PROPERTIES = [
+        'dataset', 'description', 'publisher', 'title'
+    ];
+
+    /**
+     * This property links the Catalogue with a Dataset that is part of the Catalogue.
+     * @var \PHP_DCAT_AP\DCAT\Dataset[]
+     */
+    #[URI('dcat:dataset')]
+    public array $dataset = [];
+
+    /**
+     * This property contains a free-text account of the Catalogue.
+     * This property can be repeated for parallel language versions of the description.
+     * @var \PHP_DCAT_AP\DCAT\Literal[]
+     */
+    #[URI('dct:description')]
+    public array $description = [];
+
+    /**
+     * This property refers to an entity (organisation) responsible for making the Catalogue available.
+     * @var \PHP_DCAT_AP\DCAT\Agent
+     */
+    #[URI('dct:publisher')]
+    public Agent $publisher;
+
+    /**
+     * This property contains a name given to the Catalogue.
+     * This property can be repeated for parallel language versions of the name.
+     * @var \PHP_DCAT_AP\DCAT\Literal[]
+     */
+    #[URI('dct:title')]
+    public array $title = [];
+
+    public function __construct()
+    {
+    }
 }
