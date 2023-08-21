fdfc77541fa30012973bc8cde08e1e4f5b7743bd
Michiel Stornebrink <michielstornebrink@gmail.com>
2023-08-21 17:00:05 +0200

    Add test

--- tests/test.php
+++ tests/test.php
@@ -4,14 +4,18 @@ error_reporting(E_ALL ^ E_DEPRECATED);
 
 require(__DIR__ . '/../vendor/autoload.php');
 
+use PHP_DCAT_AP\DCAT\Agent;
 use PHP_DCAT_AP\DCAT\Catalogue;
+use PHP_DCAT_AP\DCAT\Dataset;
 use PHP_DCAT_AP\DCAT\Literal;
 use PHP_DCAT_AP\Serializer\RdfSerializer;
 
 $cat = new Catalogue('test/cat/1');
 
-$cat->title[] = new Literal('Title', 'nl');
+$cat->title[] = new Literal('Smart Connected Supplier Network Vocabulary Hub ', 'nl');
 $cat->description = [new Literal('Test here', 'en'), new Literal('Hier NL', 'nl')];
+$cat->dataset[] = new Dataset();
+$cat->publisher = new Agent();
 
 $serializer = new RdfSerializer();
 
